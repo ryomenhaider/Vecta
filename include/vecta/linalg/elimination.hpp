1f8b7f74a8c3e09a86a33f27efd6565a2d298d99
ryomenhaider <onlinep680@gmail.com>
2026-08-12 18:10:00 +0500

    fixing PyPi installation

---
+++ include/vecta/linalg/elimination.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "vecta/linalg/core.hpp"
+#include "vecta/linalg/matrix.hpp"
+
+namespace vecta::linalg 
+{
+    
+    template <typename T>
+    Matrix<T> row_echelon_form(const Matrix<T>& A){
+        
+    };
+
+    template <typename T>
+    Matrix<T> reduced_row_echelon_form(const Matrix<T>& A){
+
+    };
+    
+    template <typename T>
+    Matrix<T> gaussian_elimination(const Matrix<T>& A,const T& b){
+
+    };
+
+
+} // namespace vecta::linalg
